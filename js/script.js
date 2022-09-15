c15703ecbec67621722ed02e7e2db12ede0438ce
samamoazemi <moazemisama@gmail.com>
2022-09-15 11:53:48 -0700

    convert variable types to each other

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-var userInfo = prompt("نام و نام خانوادگی خود را وارد کنید" , "Sama Moazemi")
+// string to number :
 
-alert(userInfo)
+var userAge = prompt("سن خود را وارد نمایید")   // string
+
+var userNumberAge = Number(userAge)   // becomes a number (first way)
+
+var userNumberAge = +userAge   // becomes a number (second way)
+
+alert(typeof userNumberAge)   // number
+
+
+// number to string :
+
+// var userAge = 20
+
+// var userStringAge = String(userAge)
+
+// alert(typeof userStringAge)
